8ee9a48c4b8d8110ae1601d60bf46d05c8a663a7
NonRoute <chanon.ratn@gmail.com>
2023-01-28 18:35:52 +0700

    update hospitals routes

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -2,8 +2,6 @@ const express = require('express')
 const { getHospitals, getHospital, createHospital, updateHospital, deleteHospital } = require('../controllers/hospitals')
 const router = express.Router()
 
-const app = express()
-
 router.route('/').get(getHospitals).post(createHospital)
 router.route('/:id').get(getHospital).put(updateHospital).delete(deleteHospital)
 
